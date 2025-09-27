7f7b7eb87e8c4b9d1d5b696f67dbc7be85d53bfe
Aadya Khatavkar <aadya.khatavkar@gmail.com>
2025-09-28 00:42:32 +0200

    change file names

--- render_and_push.R
+++ render_and_push.R
@@ -1,9 +1,32 @@
-# Render the RMarkdown
+# SHA256:DMkTS0WSk5jtBxATjjbLPjH1gnt9Tu5R/BO3wyYuCdQ aadya.khatavkar@gmail.com
+#      +--[ED25519 256]--+
+#      |    =B+=o        |
+#      |   oo+*=         |
+#      |  + o.*o .       |
+#      | o = ..+o.E      |
+#      |  * . .oS o . .  |
+#      | . + o  .. . + . |
+#      |  + . . +. .+ =  |
+#      |   o   = .o. + . |
+#      |       .+  ..    |
+#      +----[SHA256]-----+
+# Render the RMarkdown to HTML
+          
 rmarkdown::render("Micro1.Rmd", output_format = "html_document")
 
 # Git automation
-system("git add .")
-system("git commit -m 'Updated notes'")
+# Only add the Rmd and HTML (not every file in the folder)
+system("git add Micro1.Rmd Micro1.html")
+
+# Ask for a commit message
+msg <- readline("Commit message: ")
+
+# Commit with the custom message
+system(paste("git commit -m", shQuote(msg)))
+
+# Push to GitHub
 system("git push origin main")
 
-# source("render_and_push.R") for commit
+# Run this script in RStudio console:
+# source("render_and_push.R")
+
